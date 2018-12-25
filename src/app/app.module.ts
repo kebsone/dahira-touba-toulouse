e20e0a73ee67135ed38dbf4ff44979f3e8d53d0e
kebsone88 <smksoft@hotmail.fr>
2018-12-25 21:23:34 +0100

    create others component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,46 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { Routes, RouterModule } from '@angular/router';
+import { DahiraComponent } from './pages/dahira/dahira.component';
+import { ActualiteComponent } from './pages/actualite/actualite.component';
+import { MediathequeComponent } from './pages/mediatheque/mediatheque.component';
+import { MembreComponent } from './pages/membre/membre.component';
+import { CommerceComponent } from './pages/commerce/commerce.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { LoginComponent } from './pages/login/login.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: '', component: AccueilComponent },
+  { path: 'dahira', component: DahiraComponent },
+  { path: 'actualite', component: ActualiteComponent },
+  { path: 'mediatheque', component: MediathequeComponent },
+  { path: 'membre', component: MembreComponent },
+  { path: 'commerce', component: CommerceComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DahiraComponent,
+    ActualiteComponent,
+    MediathequeComponent,
+    MembreComponent,
+    CommerceComponent,
+    ContactComponent,
+    AccueilComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/mediatheque/mediatheque.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mediatheque',
+  templateUrl: './mediatheque.component.html',
+  styleUrls: ['./mediatheque.component.scss']
+})
+export class MediathequeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
